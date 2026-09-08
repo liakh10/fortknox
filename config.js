8676e3b97bb56dd7232512f0f57a27870f059620
liakh10 <119670997+liakh10@users.noreply.github.com>
2026-09-08 23:41:34 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* ================= CONFIG: единственное место, где меняются данные ================= */
 window.SITE = {
-  CA:      "SOON",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
+  CA:      "0xeC5396c0Df28285329a2F7e4ed6DE44b824D80F2",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
   X_URL:   "https://x.com/fortknoxcat_rh",
   NAME:    "Fort Knox Cat",
   TICKER:  "$KNOX",
